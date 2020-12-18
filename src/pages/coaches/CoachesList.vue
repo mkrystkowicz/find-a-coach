d2000d7d6fa9873aeea18490c30c40a734dcd8c5
Mateusz <krissie515@gmail.com>
2020-12-18 13:00:25 +0100

    fill up pages and check for routing work

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    filter
+  </section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul>
+      List of coaches
+    </ul>
+  </section>
+</template>
