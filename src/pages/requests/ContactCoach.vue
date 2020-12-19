21ef27da2231b7c4d56de5b134d698ef6752a789
Mateusz <krissie515@gmail.com>
2020-12-19 17:18:15 +0100

    add new request to store

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -38,6 +38,13 @@ export default {
         this.formIsValid = false;
         return;
       }
+
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.id,
+      });
+      this.$router.replace('/coaches');
     },
   },
 };
