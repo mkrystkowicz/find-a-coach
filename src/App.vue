8c4610f7637d55a2f9eac3b6203d3dd5192b0c9f
Mateusz <krissie515@gmail.com>
2020-12-23 11:25:25 +0100

    add auto-logout

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,20 @@ export default {
   components: {
     TheHeader,
   },
+  computed: {
+    didAutoLogout() {
+      return this.$store.getters.didAutoLogout;
+    },
+  },
   created() {
     this.$store.dispatch('tryLogin');
+  },
+  watch: {
+    didAutoLogout(currentValue, oldValue) {
+      if (currentValue && currentValue !== oldValue) {
+        this.$router.replace('/coaches');
+      }
+    },
   },
 };
 </script>
